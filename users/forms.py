5ddc5e9beaccd37d63dd132809c9f714326cc58e
Jackson Foster <jacksonfoster444@gmail.com>
2019-10-05 21:08:10 -0700

    fixed update password bug

--- users/forms.py
+++ users/forms.py
@@ -13,20 +13,29 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = CustomUser
         fields = ('first_name', 'last_name','email', 'username')
-    password = forms.CharField(label='Password', max_length=255, widget=forms.PasswordInput, required=False)
-    confirm_password = forms.CharField(label='Confirm Password', max_length=255, widget=forms.PasswordInput, required=False)
+    password = forms.CharField(label='Password', max_length=255, widget=forms.PasswordInput, required=False, empty_value=None)
+    confirm_password = forms.CharField(label='Confirm Password', max_length=255, widget=forms.PasswordInput, required=False, empty_value=None)
+
+    def is_valid(self):
+        valid = super(CustomUserChangeForm, self).is_valid()
+
+        if not valid:
+            return valid
+            
+        if self.cleaned_data['password'] != self.cleaned_data['confirm_password']:
+            self.add_error('password', "Password's do not match")
+            return False
+                    
+        return True
 
     def save(self, request, commit=True):
-        if self.cleaned_data['password'] != self.cleaned_data['confirm_password']:
-            raise forms.ValidationError("Password's do not match")
+        obj = super().save(commit=False)
+        if self.cleaned_data['password'] is not None:
+            obj.set_password(request.POST['password'])
+        if commit:
+            obj.save()
         else:
-            obj = super().save(commit=False)
-            if self.cleaned_data['password'] is not None:
-                obj.set_password(request.POST['password'])
-            if commit:
-                obj.save()
-            else:
-                return obj
+            return obj
 
 
 
