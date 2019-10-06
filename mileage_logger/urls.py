ef1974c12b329f367fc73a8fc498d191ea3c269d
Jackson Foster <jacksonfoster444@gmail.com>
2019-10-05 20:25:29 -0700

    updated password reset

--- mileage_logger/urls.py
+++ mileage_logger/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(),name='login'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/(<uidb64>[0-9A-Za-z_\-]+)/(<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+    path('reset/<uidb64>/<token>/',
         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
